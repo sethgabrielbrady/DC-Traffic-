532dcebacf928899d70eedf2283d1abbb9ea4a48
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-03-03 09:59:09 -0500

    parse.js is outputting an array containing arrays containig parking data element

---
+++ src/moving.js
@@ -0,0 +1,4 @@
+// MOVING.js
+// should export a single function which accepts a year and month name
+//    (not a filename) as its arguments. The function must return an object with
+//    the answers to the data analysis questions below.
