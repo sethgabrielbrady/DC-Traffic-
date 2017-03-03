6398ad9a89b5212328cc11d078eb0d508125fc5f
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-03-03 17:10:31 -0500

    Cleaned up a little more

--- src/parking.js
+++ src/parking.js
@@ -25,35 +25,34 @@ function parkingViolations(/*year, monthName*/){
 
 // console.log(ticketTypes);//this logs the correct nubmer of ticket types
 let ticketKeys = Object.keys(ticketTypes);//outputs the number of properties in an objects
-// console.log(ticketKeys);//this lof the correct number of ticketslogq
 
-// console.log(ticketKeys[2]);//this returns the key  at index 2
-// console.log(ticketTypes.P302);
-// console.log(ticketTypes[ticketKeys[2]]); // this returns the property at that key
 
-// i need to go through each ticket using a for loop, comparing the values, and
-// storing the highest value inside a teporary variable.
+/****************************************************************************/
+console.log(ticketTypes);
 
-let tempNumCont=1;
-let tempNameCont = 0;
+
+
+
+let tempNumCont=0;
+let tempNameCont = '';
 let mostTixObj= {
       ViolationType: '',
       count: 0
     };
+
 for (i=0; i<(ticketKeys.length); i++){
-  console.log(ticketKeys[i] + ':' + ticketTypes[ticketKeys[i]]);//this returns the ticket keys
-    if (tempNumCont <= ticketTypes[ticketKeys[i]] ){
-      console.log(tempNameCont + ':' +  tempNumCont);
-      tempNumCont++;
-      tempNameCont = ticketKeys[i];
+  // console.log(ticketKeys[i] + ':' + ticketTypes[ticketKeys[i]]);//
+    if (ticketTypes[ticketKeys[i]] >= tempNumCont){
+      // console.log(tempNameCont + ':' +  tempNumCont);
+      tempNumCont = ticketTypes[ticketKeys[i]];//property
+      tempNameCont = ticketKeys[i];//key
     }
 
     mostTixObj.ViolationType = tempNameCont;//stores tempNameCont inside mostTixObj
     mostTixObj.count = tempNumCont;//stores tempNumCont inside mostTixObj
 }
 console.log(mostTixObj);
-
-
+/*****************************************************************************/
 
 
 
@@ -62,6 +61,3 @@ console.log(mostTixObj);
 }
 
 parkingViolations();
-
-// I need to check all properties of an object and find out which one has the highest value
-//
